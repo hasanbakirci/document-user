6cfe655ca1a03e9ffb1f23b826cde000dcc0ba23
Hasan <hasan.bakrci@gmail.com>
2022-03-28 19:40:19 +0300

    Revert "document-service added."
    This reverts commit 67dca17d48569da0cc5f922494c2d394c7305f64.
    
    Revert "document-service added."
    
    This reverts commit 1683f30a1d690fa08cfd8360b3dc485d80cb6580.

--- src/document-service/Models/Dtos/Requests/UpdateDocumentRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace document_service.Models.Dtos.Requests;
-
-public class UpdateDocumentRequest
-{
-    public IFormFile FormFile { get; set; }
-    public string? Description { get; set; }
-}
